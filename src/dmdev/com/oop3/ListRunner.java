1e102dd7df5605f93c60503f4266a777a4a70da5
Bulat <halimov1425@gmai.com>
2026-04-22 22:30:57 +0300

    add Generics

---
+++ src/dmdev/com/oop3/ListRunner.java
@@ -0,0 +1,16 @@
+package dmdev.com.oop3;
+
+import dmdev.com.oop.Archer;
+
+public class ListRunner {
+
+    public static void main(String[] args) {
+        List<String> list = new List<>(10);
+        list.add("String1");
+        list.add("String2");
+        list.add("String3");
+
+        String element = list.get(1);
+        System.out.println(element);
+    }
+}
